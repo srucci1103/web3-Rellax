b24019d6d5a4b12c6f52cb51a9c3ece04471649a
Sarah <srucci1103@gmail.com>
2020-02-10 20:22:36 -0500

    Cleaned up

--- dev/js/rellax.js
+++ dev/js/rellax.js
@@ -2,42 +2,18 @@ var rellax = new Rellax('.rellax', {
   speed: 7,
   center: false,
   wrapper: null,
-  round: true,
   vertical: true,
-  horizontal: false
 });
 
 var rellax2 = new Rellax('.rellax2', {
   speed: 5,
   center: false,
   wrapper: null,
-  round: true,
   vertical: true,
-  horizontal: false
 });
 var rellax3 = new Rellax('.slowrellax', {
   speed: 1,
-  center: false,
+  center: 0.5,
   wrapper: null,
-  round: true,
   vertical: true,
-  horizontal: false
-});
-
-var rellax4 = new Rellax('.slowrellax2', {
-  speed: -5,
-  center: false,
-  wrapper: null,
-  round: true,
-  vertical: false,
-  horizontal: false
-});
-
-var rellax5 = new Rellax('.fastrellax', {
-  speed: 20,
-  center: false,
-  wrapper: null,
-  round: true,
-  vertical: false,
-  horizontal: false
 });
